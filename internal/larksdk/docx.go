1627f6f6c05328fc447ee9ec74934585fe71079d
rintays <rintays@users.noreply.github.com>
2026-01-30 22:14:46 +0800

    Use larksdk for docs get

--- internal/larksdk/docx.go
+++ internal/larksdk/docx.go
@@ -26,6 +26,20 @@ func (r *createDocxDocumentResponse) Success() bool {
 	return r.Code == 0
 }
 
+type getDocxDocumentResponse struct {
+	*larkcore.ApiResp `json:"-"`
+	larkcore.CodeError
+	Data *getDocxDocumentResponseData `json:"data"`
+}
+
+type getDocxDocumentResponseData struct {
+	Document *larkapi.DocxDocument `json:"document"`
+}
+
+func (r *getDocxDocumentResponse) Success() bool {
+	return r.Code == 0
+}
+
 func (c *Client) CreateDocxDocument(ctx context.Context, token string, req larkapi.CreateDocxDocumentRequest) (larkapi.DocxDocument, error) {
 	if !c.available() || c.coreConfig == nil {
 		return larkapi.DocxDocument{}, ErrUnavailable
@@ -64,6 +78,47 @@ func (c *Client) CreateDocxDocument(ctx context.Context, token string, req larka
 	}
 	if !resp.Success() {
 		return larkapi.DocxDocument{}, fmt.Errorf("create docx document failed: %s", resp.Msg)
+	}
+	if resp.Data == nil || resp.Data.Document == nil {
+		return larkapi.DocxDocument{}, nil
+	}
+	return *resp.Data.Document, nil
+}
+
+func (c *Client) GetDocxDocument(ctx context.Context, token, documentID string) (larkapi.DocxDocument, error) {
+	if !c.available() || c.coreConfig == nil {
+		return larkapi.DocxDocument{}, ErrUnavailable
+	}
+	if documentID == "" {
+		return larkapi.DocxDocument{}, errors.New("document id is required")
+	}
+	tenantToken := c.tenantToken(token)
+	if tenantToken == "" {
+		return larkapi.DocxDocument{}, errors.New("tenant access token is required")
+	}
+
+	apiReq := &larkcore.ApiReq{
+		ApiPath:                   "/open-apis/docx/v1/documents/:document_id",
+		HttpMethod:                http.MethodGet,
+		PathParams:                larkcore.PathParams{},
+		QueryParams:               larkcore.QueryParams{},
+		SupportedAccessTokenTypes: []larkcore.AccessTokenType{larkcore.AccessTokenTypeTenant, larkcore.AccessTokenTypeUser},
+	}
+	apiReq.PathParams.Set("document_id", documentID)
+
+	apiResp, err := larkcore.Request(ctx, apiReq, c.coreConfig, larkcore.WithTenantAccessToken(tenantToken))
+	if err != nil {
+		return larkapi.DocxDocument{}, err
+	}
+	if apiResp == nil {
+		return larkapi.DocxDocument{}, errors.New("get docx document failed: empty response")
+	}
+	resp := &getDocxDocumentResponse{ApiResp: apiResp}
+	if err := apiResp.JSONUnmarshalBody(resp, c.coreConfig); err != nil {
+		return larkapi.DocxDocument{}, err
+	}
+	if !resp.Success() {
+		return larkapi.DocxDocument{}, fmt.Errorf("get docx document failed: %s", resp.Msg)
 	}
 	if resp.Data == nil || resp.Data.Document == nil {
 		return larkapi.DocxDocument{}, nil
